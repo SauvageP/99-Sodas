5405a943819a15e13fb7d8b98244f8c9f8aa3234
yung305 <prgabriel88@gmail.com>
2014-10-19 14:49:30 -0400

    Initial Commit

---
+++ 99 Sodas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  99 Sodas
+//
+//  Created by Perry R Gabriel on 10/19/14.
+//  Copyright (c) 2014 Re/Up. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
